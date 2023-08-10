cc806fc1b7a5261cb3ae019996eaeaec8f62ee87
Buzgure <tudor.buzgure@stud.ubbcluj.ro>
2023-08-10 15:09:54 +0300

    implemented get Orders

--- main.js
+++ main.js
@@ -17,7 +17,9 @@ function getHomePageTemplate() {
 function getOrdersPageTemplate() {
   return `
     <div id="content">
-    <h1 class="text-2xl mb-4 mt-8 text-center">Purchased Tickets</h1>
+      <h1 class="text-2xl mb-4 mt-8 text-center">Purchased Tickets</h1>
+      <div class="orders flex items-center justify-center flex-wrap">
+      </div>
     </div>
   `;
 }
@@ -211,108 +213,71 @@ const handleAddToCart = (input, price, ticketId, date) => {
 
 }
 
-function renderHomePage() {
-  const mainContentDiv = document.querySelector('.main-content-component');
-  mainContentDiv.innerHTML = getHomePageTemplate();
-  // Sample hardcoded event data
-  const eventData = [{
-    id: 1,
-    description: 'Sample event description.',
-    img: 'https://images.unsplash.com/photo-1540575467063-178a50c2df87?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
-    name: 'Sample Event',
-    ticketCategories: [
-      { id: 1, description: 'General Admission' },
-      { id: 2, description: 'VIP' },
-    ],
-  },
-  {
-    id: 2,
-    description: 'Another event description.',
-    img: 'https://images.unsplash.com/photo-1540575467063-178a50c2df87?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
-    name: 'Another Event',
-    ticketCategories: [
-      { id: 1, description: 'Standard Ticket' },
-      { id: 2, description: 'Premium Ticket' },
-    ],
-  },
- ];
-  const eventsContainer = document.querySelector('.events');
-  eventData.forEach(event => {
-    const eventCard = document.createElement('div');
-    eventCard.classList.add('card'); 
-    
-    // Create the event content markup
-    const contentMarkup = `
-      <header class="horizontal-layout-item">
-        <h2 class="event-title text-2xl font-bold">${event.name}</h2>
-      </header>
-      <div class="horizontal-layout-item vertical-layout">
-        <img src="${event.img}" alt="${event.name}" class="vertical-layout-item img rounded object-cover mb-4"/>
-        <p class="vertical-layout-item text">${event.description}</p>
-        <label class="vertical-layout-item tickets ">Select ticket category:
-      </label>
-      </div>
-      
-    `;
-    eventCard.innerHTML = contentMarkup;
-    const ticketData = event.ticketCategories;
-    const ticketCard = document.createElement('select');
-    ticketCard.classList.add('ticket-select');
-    
-    // Add a title or label option
-    const titleOption = document.createElement('option');
-    titleOption.disabled = true;
-    titleOption.selected = true;
-    titleOption.textContent = 'Select Ticket Category';
-    ticketCard.appendChild(titleOption);
-
-    ticketData.forEach(ticket => {
-      const option = document.createElement('option');
-      option.value = ticket.description;
-      option.textContent = ticket.description;
-      ticketCard.appendChild(option);
-    });
-
-    const ticketContainer = eventCard.querySelector('.tickets');
-    ticketContainer.appendChild(ticketCard);
-    const quantitySelector = document.createElement('input');
-    quantitySelector.type = 'number';
-    quantitySelector.min = 0;
-    quantitySelector.value = 0;
-    quantitySelector.classList.add('quantity-selector');
-
-    const addToCartButton = document.createElement('button');
-    addToCartButton.textContent = 'Add to Cart';
-    addToCartButton.classList.add('add-to-cart-button');
-    addToCartButton.style.display = 'none';
-
-    quantitySelector.addEventListener('input', () => {
-      const quantity = parseInt(quantitySelector.value);
-      addToCartButton.style.display = quantity > 0 ? 'block' : 'none';
-    });
-
-    // Event listener for adding to cart
-    addToCartButton.addEventListener('click', () => {
-      const selectedTicket = ticketCard.value;
-      const selectedQuantity = parseInt(quantitySelector.value);
-      if (selectedQuantity > 0) {
-        console.log(`Added ${selectedQuantity} ${selectedTicket} for ${event.name} tickets to cart.`);
-      }
-    });
-    eventCard.appendChild(quantitySelector);
-    eventCard.appendChild(addToCartButton);
-    eventsContainer.appendChild(eventCard);
-
-
-  })
-  // Create the event card element
-
-  
-}
 function renderOrdersPage(categories) {
   const mainContentDiv = document.querySelector('.main-content-component');
   mainContentDiv.innerHTML = getOrdersPageTemplate();
+  console.log('function', fetchOrders());
+  fetchOrders().then((data) => {
+    addOrders(data);
+  });
+
 }
+
+async function fetchOrders(){
+  const response = await fetch('https://localhost:7203/api/Order/getAllOrdersByCustomerId?id=4');
+  const data = await response.json();
+  return data;
+
+
+}
+
+const addOrders = ((orders) => {
+  const orderDiv = document.querySelector('.orders');
+  orderDiv.innerHTML = 'No orders';
+  if (orders.length){
+    orderDiv.innerHTML ='';
+    orders.forEach(order => {
+      orderDiv.appendChild(createOrder(order));
+    })
+    
+  }
+})
+
+const createOrder = ((orderData) => {
+  const title = orderData.name;
+  const orderElement = createOrderElement(orderData);
+  console.log(orderElement);
+  return orderElement;
+
+})
+
+const createOrderElement = ((orderData) => {
+ const {orderId, customerName, orderedAt, numberOfTickets, totalPrice} = orderData;
+ const orderDiv = document.createElement('div');
+ orderDiv.classList.add('card');
+  const contentMarkup = `
+      <header class="vertical-layout-item">
+        <h2 class="order-title text-2xl font-bold">${orderId}</h2>
+      </header>
+      <div class="vertical-layout-item order-vertical-layout">
+        <p class="vertical-layout-item text order-p ">${customerName}</p>
+        <p class="vertical-layout-item text order-p ">${orderedAt}</p>
+        <div class = "order-horizontal-layout">
+        <p class="horizontal-layout-item order-text p">No of tickets:</p>
+        <p class="horizontal-layout-item text p order-text-gray-700">${numberOfTickets}</p>
+        </div>
+        <div class = "horizontal-layout">
+        <p class="horizontal-layout-item order-text p">Price: </p>
+        <p class="horizontal-layout-item text p order-text-gray-700">${totalPrice}</p>
+        </div>
+        
+      </div>
+      
+    `;
+    orderDiv.innerHTML = contentMarkup;
+  return orderDiv;
+
+})
 
 // Render content based on URL
 function renderContent(url) {
